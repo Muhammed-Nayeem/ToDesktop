59f7a50968f8343e82222d3a2c7f2fca990fa973
Muhammed Nayeem <mohammednayeem808@gmail.com>
2025-06-18 13:05:00 +0600

    Scroll Listener: Scroll Slider

--- Assets/JavaScripts/app.js
+++ Assets/JavaScripts/app.js
@@ -1,4 +1,42 @@
+//Responsive Nav menu toggle:
 const navbarDialog = document.getElementById("navbar-dialog");
 const handleMobileMenu = () => {
   navbarDialog.classList.toggle("hidden");
 };
+
+//Scroll listener and slider animation:
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 40 * 4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+  const intersectionCallback = (entries) => {
+    let isIntersecting = entries[0].isIntersecting;
+    if (isIntersecting) {
+      document.addEventListener("scroll", scrollHandler);
+    } else {
+      document.removeEventListener("scroll", scrollHandler);
+    }
+  };
+
+  const intersectionObserver = new IntersectionObserver(intersectionCallback);
+  intersectionObserver.observe(element);
+
+  function scrollHandler() {
+    let translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+    let totalTranslate = 0;
+    if (isLTR) {
+      totalTranslate = translateX + initialTranslateLTR;
+    } else {
+      totalTranslate = -(translateX + initialTranslateRTL);
+    }
+    element.style.transform = `translateX(${totalTranslate}px)`;
+  }
+}
+
+const group_1 = document.getElementById("group-1");
+const group_2 = document.getElementById("group-2");
+const group_3 = document.getElementById("group-3");
+
+setupIntersectionObserver(group_1, true, 0.15);
+setupIntersectionObserver(group_2, false, 0.15);
+setupIntersectionObserver(group_3, true, 0.15);
